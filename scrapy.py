51abf13420846a065c6a04a0cb49535e3290e17c
nandodevs <nando.devs@gmail.com>
2024-03-07 15:25:09 -0300

    Code scrapping

---
+++ scrapy.py
@@ -0,0 +1,59 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/api/top-dramas', methods=['GET'])
+def get_top_dramas():
+    # Lista para armazenar os resultados
+    dramas = []
+
+    # Iterar sobre as 20 primeiras páginas
+    for page in range(1, 3):
+        # URL do site
+        url = f"https://br.mydramalist.com/shows/top?page={page}"
+
+        # Requisição HTTP
+        response = requests.get(url)
+
+        # Verificação de sucesso
+        if response.status_code == 200:
+            # Criação do objeto BeautifulSoup
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            # Busca por elementos que contêm os dados
+            bloco_dramas = soup.find_all("a", class_="block")
+
+            # Iterar sobre os dados encontrados
+            for bloco in bloco_dramas:
+                # Extrair informações
+                link_dorama = bloco['href']
+                
+                # Utilizando o atributo data-src para obter a imagem
+                link_imagem_elemento = bloco.find("img", class_="img-responsive")
+                link_imagem = link_imagem_elemento['data-src'].replace("s.jpg", "c.jpg") if link_imagem_elemento else ""
+
+                titulo_elemento = bloco.find_next("h6", class_="text-primary title")
+                nome_dorama = titulo_elemento.text.strip() if titulo_elemento else ""
+
+                classificacao_elemento = bloco.find_next("span", class_="p-l-xs score")
+                classificacao_valor = classificacao_elemento.text.strip() if classificacao_elemento else ""
+
+                # Adicionar os dados à lista de dramas
+                drama = {
+                    "Imagem": link_imagem,
+                    "Título": nome_dorama,
+                    "Link": link_dorama,
+                    "Classificação": classificacao_valor
+                }
+                dramas.append(drama)
+
+        else:
+            return jsonify({"error": f"Erro ao acessar o site: {response.status_code}"})
+
+    # Retorna os resultados como JSON em UTF-8
+    return jsonify(dramas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
