697f653f7fcfc4133debff9b30dccf4111ea5bcd
Nando Jr <nando.devs@gmail.com>
2024-03-19 21:57:38 -0300

    Modified API Classification and Create New API

---
+++ api/doramas-series.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def scrape_doramas(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    doramas = []
+
+    for item in soup.find_all('div', class_='item'):
+        dorama = {}
+        dorama['title'] = item.find('h2').text.strip()
+        dorama['image'] = item.find('img')['src']
+        dorama['year'] = item.find('span', class_='date').text.strip()
+        dorama['link'] = item.find('a')['href']
+        doramas.append(dorama)
+
+    return doramas
+
+@app.route('/doramas/legendado', methods=['GET'])
+def get_legendado_doramas():
+    url = 'https://doramasonline.org/br/generos/legendado/'
+    doramas = scrape_doramas(url)
+    return jsonify(doramas)
+
+
+@app.route('/doramas/dublado', methods=['GET'])
+def get_dublado_doramas():
+    url = 'https://doramasonline.org/br/generos/dublado/'
+    doramas = scrape_doramas(url)
+    return jsonify(doramas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
