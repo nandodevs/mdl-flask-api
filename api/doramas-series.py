4f452c4cf172073ec469680625ae29c763d599f4
nandodevs <nando.devs@gmail.com>
2024-03-21 14:36:07 -0300

    Update Series Api

--- api/doramas-series.py
+++ api/doramas-series.py
@@ -4,31 +4,36 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-def scrape_doramas(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+def scrape_doramas(url_template, total_pages):
     doramas = []
 
-    for item in soup.find_all('article', class_='item movies'):
-        dorama = {}
-        dorama['title'] = item.find('h3').text.strip()
-        dorama['image'] = item.find('img')['src']
-        dorama['year'] = item.find('span').text.strip()
-        dorama['link'] = item.find('a')['href']
-        doramas.append(dorama)
+    for page_num in range(1, total_pages + 1):
+        url = url_template.format(page_num)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for item in soup.find_all('article', class_='item movies'):
+            dorama = {}
+            dorama['title'] = item.find('h3').text.strip()
+            dorama['image'] = item.find('img')['src']
+            dorama['year'] = item.find('span').text.strip()
+            dorama['link'] = item.find('a')['href']
+            doramas.append(dorama)
 
     return doramas
 
-@app.route('/api/series-legendado', methods=['GET'])
+@app.route('/api/doramas/legendado', methods=['GET'])
 def get_legendado_doramas():
-    url = 'https://doramasonline.org/br/generos/legendado/'
-    doramas = scrape_doramas(url)
+    url_template = 'https://doramasonline.org/br/generos/legendado/page/{}/'
+    total_pages = 25
+    doramas = scrape_doramas(url_template, total_pages)
     return jsonify(doramas)
 
-@app.route('/api/series-dublado', methods=['GET'])
+@app.route('/api/doramas/dublado', methods=['GET'])
 def get_dublado_doramas():
-    url = 'https://doramasonline.org/br/generos/dublado/'
-    doramas = scrape_doramas(url)
+    url_template = 'https://doramasonline.org/br/generos/dublado/page/{}/'
+    total_pages = 25
+    doramas = scrape_doramas(url_template, total_pages)
     return jsonify(doramas)
 
 if __name__ == '__main__':
